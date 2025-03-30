5d3cf4bbc767ee4f70c402488ee70bdef197aa5c
MvLeronn <marcos23_correia@hotmail.com>
2025-03-30 18:05:30 -0300

    feat(cors): permitir todas as origens com credenciais habilitadas

--- backend/index.js
+++ backend/index.js
@@ -5,19 +5,34 @@ const UserController = require("./controllers/UserController");
 
 const app = express();
 
-// Config JSON Response
+// Configura JSON Response
 app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
 
-// Solve CORS
+// Resolve problemas de CORS
+app.use(
+  cors({
+    credentials: true,
+    origin: (origin, callback) => {
+      if (origin) {
+        callback(null, true); // Permite qualquer origem
+      } else {
+        callback(new Error("Origem não permitida pelo CORS"));
+      }
+    },
+  })
+);
+
+/* Permite apenas 2 origens
 app.use(
   cors({
     credentials: true,
     origin: ["http://localhost:5173", "http://127.0.0.1:5500"],
   })
 );
+*/
 
-// wait for the database connection to initialize the admin user
+// Aguarda a conexão com o banco de dados antes de criar o usuário admin
 mongoose.connection.once("open", () => {
   console.log("Conexão com o banco de dados estabelecida.");
   UserController.initializeAdminUser();
@@ -30,4 +45,5 @@ const PerguntaRoutes = require("./routes/PerguntaRoutes");
 app.use("/users", UserRoutes);
 app.use("/perguntas", PerguntaRoutes);
 
+// Inicia o servidor na porta 5000
 app.listen(5000);
